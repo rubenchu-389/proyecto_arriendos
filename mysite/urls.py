11100f384312d7817c820d031461b2852b584ef4
rubenchu-389 <innovaservices2022@gmail.com>
2024-09-07 12:50:26 -0400

    Se crean los modelos con sus relaciones

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('m7_python.urls')),
+    path('', include('m7_python.urls')), # nombre de la aplicacion 
+    
 ]
