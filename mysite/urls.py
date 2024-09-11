7a2f18aae02adde4e8ae36544047a0dfa46f7930
rubenchu-389 <innovaservices2022@gmail.com>
2024-09-11 16:44:46 -0300

    Elaboracion del LOGIN/LOGOUT

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('m7_python.urls')), # nombre de la aplicacion 
-    
+    path('', include('m7_python.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+   
 ]
